1f7edadfeda89d0f3f4b6c3d71734478b4b6cb88
David M. Syzdek <syzdek@bindlebinaries.com>
2012-08-24 09:03:04 -0800

    Adding new project.

---
+++ IDMDemo/IDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IDAppDelegate.h
+//  IDMDemo
+//
+//  Created by David M. Syzdek on 8/24/12.
+//  Copyright (c) 2012 Bindle Binaries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IDMDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IDMDemo
+//
+//  Created by David M. Syzdek on 8/24/12.
+//  Copyright (c) 2012 Bindle Binaries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+   @autoreleasepool {
+       return UIApplicationMain(argc, argv, nil, NSStringFromClass([IDAppDelegate class]));
+   }
+}
